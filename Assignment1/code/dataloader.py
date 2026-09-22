26b9103e4427127390cbbd892b9cff9132480528
PhuocSang44 <phuocsang4405@gmail.com>
2026-09-23 00:57:42 +0700

    propose fix dataleak, add draft report

--- Assignment1/code/dataloader.py
+++ Assignment1/code/dataloader.py
@@ -7,46 +7,62 @@ from torch.utils.data import DataLoader, Subset
 from torchvision.datasets import FashionMNIST
 from torchvision.transforms import Compose, Normalize, RandomAffine, RandomHorizontalFlip, ToTensor
 
-from config import MEAN_TUP, SD_TUP, DATA_PATH, VAL_SIZE, SEED, BATCH_SIZE, NUM_WORKERS
+from config import DATA_PATH, VAL_SIZE, SEED, BATCH_SIZE, NUM_WORKERS
 
 class FashionMNISTDataLoader():
     def __init__(self, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS, use_augmentation=True):
         self.train_generator = torch.Generator()
         self.train_generator.manual_seed(SEED)
 
-        val_transform = [ToTensor(), Normalize(MEAN_TUP, SD_TUP)]
-        train_transform = []
-        if use_augmentation:
-            train_transform = [
-                RandomHorizontalFlip(p=0.5),
-                RandomAffine(degrees=(-10, 10),translate=(0.1, 0.1), scale=(0.9, 1.1))
-            ]
+        train_data = FashionMNIST(
+            root=DATA_PATH,
+            train=True,
+            download=True,
+            transform=None,
+            target_transform=None,
+        )
 
-        self.test_set = FashionMNIST(root=DATA_PATH,
-                                     train=False,
-                                     download=True,
-                                     transform=Compose(val_transform),
-                                     target_transform=None)
-
-        self.val_set = FashionMNIST(root=DATA_PATH,
-                                   train=True,
-                                   download=True,
-                                   transform=Compose(val_transform),
-                                   target_transform=None)
-
-        self.train_set = FashionMNIST(root=DATA_PATH,
-                                      train=True,
-                                      download=True,
-                                      transform=Compose(train_transform + val_transform),
-                                      target_transform=None)
-
-        indices = numpy.arange(len(self.train_set))
-        targets = numpy.array(self.train_set.targets)
+        indices = numpy.arange(len(train_data))
+        targets = numpy.array(train_data.targets)
 
         train_idx, val_idx = train_test_split(indices,
                                               test_size=VAL_SIZE,
                                               random_state=SEED,
                                               stratify=targets)
+
+        train_pixels = train_data.data[train_idx].float() / 255.0
+        train_mean = train_pixels.mean().item()
+        train_std = train_pixels.std().item()
+        val_transform = [ToTensor(), Normalize((train_mean,), (train_std,))]
+        train_transform = []
+        if use_augmentation:
+            train_transform = [
+                RandomHorizontalFlip(p=0.5),
+                RandomAffine(
+                    degrees=(-10, 10),
+                    translate=(0.1, 0.1),
+                    scale=(0.9, 1.1),
+                ),
+            ]
+
+        self.test_set = FashionMNIST(
+            root=DATA_PATH,
+            train=False,
+            download=True,
+            transform=Compose(val_transform),
+            target_transform=None,
+        )
+
+        self.val_set = FashionMNIST(
+            root=DATA_PATH,
+            train=True,
+            download=True,
+            transform=Compose(val_transform),
+            target_transform=None,
+        )
+
+        train_data.transform = Compose(train_transform + val_transform)
+        self.train_set = train_data
 
         self.train_set = Subset(self.train_set, train_idx)
         self.val_set = Subset(self.val_set, val_idx)
